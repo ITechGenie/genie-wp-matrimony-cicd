660f7423c4cdab31e1b6a7496471590bf51a4e1e
Prakash <prakashm88@yahoo.co.in>
2021-03-20 14:06:11 +0530

    update to folked version - to allow future updates

--- config/environments/staging.php
+++ config/environments/staging.php
@@ -1,29 +1,15 @@
 <?php
-/** Staging */
-ini_set('display_errors', 0);
-define('WP_DEBUG_DISPLAY', false);
-define('SCRIPT_DEBUG', false);
-/** Disable all file modifications including updates and update notifications */
-define('DISALLOW_FILE_MODS', true);
+/**
+ * Configuration overrides for WP_ENV === 'staging'
+ */
 
+use Roots\WPConfig\Config;
 
 /**
- * DB settings
+ * You should try to keep staging as close to production as possible. However,
+ * should you need to, you can always override production configuration values
+ * with `Config::define`.
+ *
+ * Example: `Config::define('WP_DEBUG', true);`
+ * Example: `Config::define('DISALLOW_FILE_MODS', false);`
  */
-$url = parse_url(env('JAWSDB_MARIA_URL'));
-putenv(sprintf('DB_NAME=%s', substr($url['path'], 1)));
-putenv(sprintf('DB_USER=%s', $url['user']));
-putenv(sprintf('DB_PASSWORD=%s', $url['pass']));
-putenv(sprintf('DB_HOST=%s', $url['host']));
-unset($url);
-
-/**
- * Memcached settings
- */
-define('WP_CACHE', true);
-global $memcached_servers, $memcached_username, $memcached_password;
-$memcached_servers  = array_map(function ($server) {
-    return explode(':', $server, 2);
-}, explode(',', env('MEMCACHEDCLOUD_SERVERS')));
-$memcached_username = env('MEMCACHEDCLOUD_USERNAME');
-$memcached_password = env('MEMCACHEDCLOUD_PASSWORD');
